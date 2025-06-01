c176bbba54314ec3f3200df1202cee1553a00e44
mjkohan <mjk1551381@gmail.com>
2025-06-01 20:54:09 +0330

    feat: add svgr

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    turbopack: {
+        rules: {
+            '*.svg': {
+                loaders: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            svgoConfig: {
+                                plugins: [
+                                    {
+                                        name: 'preset-default',
+                                        params: {
+                                            overrides: {
+                                                // customize default plugin options
+                                                removeViewBox: false,
+                                            },
+                                        },
+                                    },
+                                    'removeDimensions',
+                                ],
+                            }
+                        },
+                    },
+                ],
+                as: '*.js',
+            },
+        },
+    },
+}
 
 export default nextConfig;
+
--- src/components/hero/TechStack.tsx
+++ src/components/hero/TechStack.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import React from 'react';
@@ -7,43 +8,38 @@ import {
     TooltipProvider,
     TooltipTrigger,
 } from '@/components/ui/tooltip';
-import {
-    FaReact,
-    FaNodeJs,
-    FaBootstrap,
-    FaGit,
-    FaDocker,
-    FaHtml5,
-    FaCss3Alt,
-} from 'react-icons/fa';
-import {
-    SiTypescript,
-    SiNextdotjs,
-    SiMui,
-    SiRedux,
-    SiJavascript,
-} from 'react-icons/si';
-import { RiTailwindCssFill } from 'react-icons/ri';
 
-type TechItem = {
-    name: string;
-    icon: React.ElementType;
-};
 
-const techStack: TechItem[] = [
-    { name: 'HTML5', icon: FaHtml5 },
-    { name: 'CSS3', icon: FaCss3Alt },
-    { name: 'JavaScript', icon: SiJavascript },
-    { name: 'TypeScript', icon: SiTypescript },
-    { name: 'React', icon: FaReact },
-    { name: 'Next.js', icon: SiNextdotjs },
-    { name: 'Redux', icon: SiRedux },
-    { name: 'Node.js', icon: FaNodeJs },
-    { name: 'Tailwind CSS', icon: RiTailwindCssFill },
-    { name: 'Bootstrap', icon: FaBootstrap },
-    { name: 'MUI', icon: SiMui },
-    { name: 'Git', icon: FaGit },
-    { name: 'Docker', icon: FaDocker },
+import Html5Icon from '../../../public/logos/html5.svg';
+import Css3Icon from '../../../public/logos/css.svg';
+import JavascriptIcon from '../../../public/logos/javascript.svg';
+import TypescriptIcon from '../../../public/logos/typescript.svg';
+import ReactIcon from '../../../public/logos/react.svg';
+import NextjsIcon from '../../../public/logos/nextdotjs.svg';
+import ReduxIcon from '../../../public/logos/redux.svg';
+import NodejsIcon from '../../../public/logos/nodedotjs.svg';
+import TailwindcssIcon from '../../../public/logos/tailwindcss.svg';
+import BootstrapIcon from '../../../public/logos/bootstrap.svg';
+import MuiIcon from '../../../public/logos/mui.svg';
+import GitIcon from '../../../public/logos/git.svg';
+import DockerIcon from '../../../public/logos/docker.svg';
+import GraphqlIcon from '../../../public/logos/graphql.svg';
+
+const techStack = [
+    { name: 'HTML5', Icon: Html5Icon, color: '#E34F26' },
+    { name: 'CSS3', Icon: Css3Icon, color: '#1572B6' },
+    { name: 'JavaScript', Icon: JavascriptIcon, color: '#F7DF1E' },
+    { name: 'TypeScript', Icon: TypescriptIcon, color: '#3178C6' },
+    { name: 'React', Icon: ReactIcon, color: '#61DAFB' },
+    { name: 'Next.js', Icon: NextjsIcon, color: '#000000' },
+    { name: 'Redux', Icon: ReduxIcon, color: '#764ABC' },
+    { name: 'Node.js', Icon: NodejsIcon, color: '#5FA04E' },
+    { name: 'Tailwind CSS', Icon: TailwindcssIcon, color: '#06B6D4' },
+    { name: 'Bootstrap', Icon: BootstrapIcon, color: '#7952B3' },
+    { name: 'MUI', Icon: MuiIcon, color: '#007FFF' },
+    { name: 'Git', Icon: GitIcon, color: '#F05032' },
+    { name: 'Docker', Icon: DockerIcon, color: '#2496ED' },
+    { name: 'GraphQL', Icon: GraphqlIcon, color: '#E10098' },
 ];
 
 export function TechStack() {
@@ -52,25 +48,21 @@ export function TechStack() {
     return (
         <div className="overflow-hidden w-full max-w-full">
             <div className="flex whitespace-nowrap animate-[marquee_20s_linear_infinite]">
-                {duplicatedStack.map((tech, index) => {
-                    const IconComponent = tech.icon;
-                    return (
-                        <TooltipProvider key={`${tech.name}-${index}`}>
-                            <Tooltip>
-                                <TooltipTrigger asChild>
-                                    <div className="inline-flex w-12 h-12 mx-2 items-center justify-center rounded-md bg-secondary p-2 transition-transform hover:scale-110">
-                                        <IconComponent className="text-5xl" />
-                                    </div>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>{tech.name}</p>
-                                </TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
-                    );
-                })}
+                {duplicatedStack.map(({ name, Icon, color }, index) => (
+                    <TooltipProvider key={`${name}-${index}`}>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <div className="inline-flex bg-white  w-12 h-12 mx-2 items-center justify-center rounded-md p-2 transition-transform hover:scale-110 flex-shrink-0">
+                                    <Icon fill={color} className={"w-8 h-8 " }  />
+                                </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>{name}</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                ))}
             </div>
         </div>
     );
 }
-
